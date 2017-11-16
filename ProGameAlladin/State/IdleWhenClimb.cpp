61c2fa9efd29007a8189d6875d33e074a0552771
Lê Võ Gia Khang <levogiakhang@gmail.com>
2017-11-16 18:30:05 +0700

    Add new State

---
+++ ProGameAlladin/State/IdleWhenClimb.cpp
@@ -0,0 +1,24 @@
+﻿#include "IdleWhenClimb.h"
+US_NS_JK
+
+IdleWhenClimb::IdleWhenClimb(Node* node):State(node)
+{
+}
+
+IdleWhenClimb::~IdleWhenClimb()
+{
+}
+
+void IdleWhenClimb::onEnter()
+{
+}
+
+State* IdleWhenClimb::checkTransition()
+{
+	if (Input::getInstance()->getKey(KEY_S))
+		return new SlashWhenClimb(_node);
+	if (Input::getInstance()->getKey(KEY_A))
+		return new ThrowWhenClimb(_node);
+
+	return nullptr;
+}
---
+++ ProGameAlladin/State/SlashWhenClimb.cpp
@@ -0,0 +1,24 @@
+﻿#include "SlashWhenClimb.h"
+US_NS_JK
+
+SlashWhenClimb::SlashWhenClimb(Node* node):State(node)
+{
+}
+
+SlashWhenClimb::~SlashWhenClimb()
+{
+}
+
+void SlashWhenClimb::onEnter()
+{
+}
+
+State* SlashWhenClimb::checkTransition()
+{
+	if (Input::getInstance()->getKey(KEY_D))
+		return new JumpWhileClimb(_node);
+	if (Input::getInstance()->getKey(KEY_A))
+		return new ThrowWhenClimb(_node);
+
+	return nullptr;
+}
