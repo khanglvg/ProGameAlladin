2a49cc5ed4135f518f284e162c73d0f5191797a2
Lê Võ Gia Khang <levogiakhang@gmail.com>
2017-11-16 17:57:22 +0700

    Add State comment

--- ProGameAlladin/State/StateInclude.h
+++ ProGameAlladin/State/StateInclude.h
@@ -7,34 +7,34 @@
 #include "../Framework/Node.h"
 
 #include "State.h"
-
-#include "Idle.h"
-#include "Run.h"
-#include "Throw.h"
-#include "Slash.h"
-#include "Jump.h"
-#include "Climb.h"
-#include "RunAndJump.h"
-#include "HeadUp.h"
-#include "JumpAndSlash.h"
-#include "JumpAndThrow.h"
-#include "Push.h"
-#include "HeadUpAndSlash.h"
-#include "Sit.h"
-#include "SitAndThrow.h"
-#include "SitAndSlash.h"
-#include "RunAndSlash.h"
-#include "Fall.h"
-#include "Brake.h"
-#include "Wing.h"
-#include "IdleWhenWing.h"
-#include "ThrowWhenWing.h"
-#include "SlashWhenWing.h"
-#include "JumpWhileClimb.h"
-#include "BeingAttacked.h"
-#include "BeingAttackedAndDie.h"
-#include "BeingRescued.h"
-#include "RunAndThrow.h"
+											// STATE
+#include "Idle.h"							// 1			
+#include "Run.h"							// 2
+#include "Throw.h"							// 3
+#include "Slash.h"							// 4
+#include "Jump.h"							// 5
+#include "Climb.h"							// 6
+#include "RunAndJump.h"						// 7
+#include "HeadUp.h"							// 8
+#include "JumpAndSlash.h"					// 9
+#include "JumpAndThrow.h"					// 10
+#include "Push.h"							// 11
+#include "HeadUpAndSlash.h"					// 12
+#include "Sit.h"							// 13
+#include "SitAndThrow.h"					// 14
+#include "SitAndSlash.h"					// 15					
+#include "RunAndSlash.h"					// 16
+#include "Fall.h"							// 17
+#include "Brake.h"							// 18
+#include "Wing.h"							// 19
+#include "IdleWhenWing.h"					// 20
+#include "ThrowWhenWing.h"					// 21
+#include "SlashWhenWing.h"					// 22
+#include "JumpWhileClimb.h"					// 23
+#include "BeingAttacked.h"					// 24
+#include "BeingAttackedAndDie.h"			// 25
+#include "BeingRescued.h"					// 26
+#include "RunAndThrow.h"					// 27
 
 
 #endif __STATEINCLUDE_H__
